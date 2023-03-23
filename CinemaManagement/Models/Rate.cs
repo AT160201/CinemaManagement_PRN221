706fd9e1802d9a4aba275b9ef36aac7e67f5e145
AT160201 <thutuoitd@gmail.com>
2023-03-23 16:34:21 +0700

    Recreate new git

---
+++ CinemaManagement/Models/Rate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaManagement.Models
+{
+    public partial class Rate
+    {
+        public int MovieId { get; set; }
+        public int PersonId { get; set; }
+        [Required(ErrorMessage = "Hãy điền comment")]
+        public string Comment { get; set; }
+        [Required(ErrorMessage = "Hãy điền điểm đánh giá")]
+        public double? NumericRating { get; set; }
+        public DateTime? Time { get; set; }
+
+        public virtual Movie Movie { get; set; }
+        public virtual Person Person { get; set; }
+    }
+}
