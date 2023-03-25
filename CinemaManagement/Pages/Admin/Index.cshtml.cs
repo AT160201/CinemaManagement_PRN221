acaea6bd4dd115ea415407ba9e17a354ebae0040
AT160201 <thutuoitd@gmail.com>
2023-03-25 10:49:06 +0700

    Add pagination

--- CinemaManagement/Pages/Admin/Index.cshtml.cs
+++ CinemaManagement/Pages/Admin/Index.cshtml.cs
@@ -28,13 +28,19 @@ namespace CinemaManagement.Pages.Admin
         public int PageSize = 3;
 
         public int TotalPages { get; set; }
+
+        public int CurrentPage2 { get; set; } = 1;
+
+        public int PageSize2 = 3;
+
+        public int TotalPages2 { get; set; }
         public void OnGet()
         {
             totalCategory = _db.Genres.Count();
             totalMember = _db.Persons.Count();
             totalMovie = _db.Movies.Count();
             totalRate = _db.Rates.Count();
-            genres = _db.Genres.Include("Movies").ToList();
+            genres = _db.Genres.Include("Movies").Skip((CurrentPage2 - 1) * PageSize2).Take(PageSize2).ToList();
             rates = _db.Rates.Include("Person").Skip((CurrentPage - 1) * PageSize).Take(PageSize).ToList();
             movies = _db.Movies.ToList();
             movie = null;
@@ -42,6 +48,11 @@ namespace CinemaManagement.Pages.Admin
             if (_db.Rates.ToList().Count() <= PageSize)
             {
                 TotalPages = 0;
+            }
+            TotalPages2 = _db.Genres.ToList().Count() / PageSize2;
+            if (_db.Genres.ToList().Count() <= PageSize2)
+            {
+                TotalPages2 = 0;
             }
         }
 
@@ -55,12 +66,18 @@ namespace CinemaManagement.Pages.Admin
                 CurrentPage = int.Parse(page);
             }
             else { CurrentPage = 1; }
+            string page2 = Request.Form["page2"];
+            if (page2 != null)
+            {
+                CurrentPage2 = int.Parse(page2);
+            }
+            else { CurrentPage2 = 1; }
             totalCategory = _db.Genres.Count();
             totalMember = _db.Persons.Count();
             totalMovie = _db.Movies.Count();
             totalRate = _db.Rates.Count();
-            genres = _db.Genres.Include("Movies").ToList();
-            if(int.Parse(mId) == 0)
+            genres = _db.Genres.Include("Movies").Skip((CurrentPage2 - 1) * PageSize2).Take(PageSize2).ToList();
+            if (int.Parse(mId) == 0)
             {
                 rates = _db.Rates.Include("Person").Skip((CurrentPage - 1) * PageSize).Take(PageSize).ToList();
                 movie = null;
@@ -78,7 +95,12 @@ namespace CinemaManagement.Pages.Admin
                 TotalPages = 0;
             }
             movies = _db.Movies.ToList();
-            
+
+            TotalPages2 = _db.Genres.ToList().Count() / PageSize2;
+            if (_db.Genres.ToList().Count() <= PageSize2)
+            {
+                TotalPages2 = 0;
+            }
         }
     }
 }
---
+++ CinemaManagement/Pages/Admin/Test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CinemaManagement.Pages.Admin
+{
+    public class TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
